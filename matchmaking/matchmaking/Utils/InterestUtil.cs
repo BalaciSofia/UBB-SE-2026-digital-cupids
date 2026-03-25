105d5f80222c4a2611b092f71720e47c1b2b8b26
davidberbecar30 <berbecardavid681@gmail.com>
2026-03-25 21:04:09 +0300

    Implemented InterestUtil and MockUserUtil

---
+++ matchmaking/matchmaking/Utils/InterestUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace matchmaking.Utils
+{
+    internal class InterestUtil
+    {
+        private string interestsFile;
+        private List<String> interests;
+       
+        public InterestUtil()
+        {
+            interests = new List<String>();
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            interestsFile = Path.Combine(baseDirectory, "interests.txt");
+
+            if (File.Exists(interestsFile))
+            {
+                String allLines = File.ReadAllText(interestsFile);
+                interests = allLines.Split(',').Select(i=>i.Trim().ToLower()).Where(i=>!string.IsNullOrEmpty(i)).ToList();
+            }
+            else
+            {
+                throw new Exception("The file doesn't exist");
+            }
+        }
+
+        public List<String> GetAll()
+        {
+            return interests;
+        }
+
+    }
+}
